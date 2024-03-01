7a31ccdbadba630be448efcf1ed08daf98d53f42
Collin Padgett <collinpadgett@pop-os.localdomain>
2024-02-29 18:12:24 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    return (
+	<div id="wrapper">
+	    <h1>Image goes here.</h1>
+	    <input type="text" placeholder="Username" />
+	    <br />
+	    <br />
+	    <input type="text" placeholder="Password" />
+	    <br />
+	    <br />
+	    <button>Submit Button</button>
+	</div>
   );
 }
 
